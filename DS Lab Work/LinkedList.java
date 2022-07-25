41cb4fb1d83b1b99653ad70a61cc002890621d73
Dhruv Karavadiya <94845676+DhruvKaravadiya@users.noreply.github.com>
2022-07-25 11:27:20 +0530

    Update LinkedList.java

--- DS Lab Work/LinkedList.java
+++ DS Lab Work/LinkedList.java
@@ -1,60 +1,154 @@
-class Link{
+// Linked list operations in Java
 
-	static Node head;
-	static class Node{
-		int value;
-		Node next;
-		Node(int n){
-			value=n;
-			next=null;
-		}
-	}
-	static void insert(int m){
-		Node mNode = new Node(m);
-		if(head==null){
-			head=mNode;
-		}
-		else{
-			Node lastNode = head;
-			while(lastNode.next!=null){
-				lastNode=lastNode.next;
-			}
-			lastNode.next=mNode;
-		}
-	}
+class LinkedList {
+  Node head;
 
-	static void display(){
-		Link lastNode2=new Link();
-		if(head==null){
-			System.out.println("Empty");
-		}
-		else{
-			System.out.print("\nLinked List :");
-			Node lastNode = head;
-			while(lastNode.next!=null){
-				System.out.print(lastNode.value+" => ");
-				lastNode=lastN
-				\
-				ode.next;
-			}
-			System.out.print(lastNode.value);
-			
-		}
-	}
-}
+  // Create a node
+  class Node {
+    int data;
+    Node next;
 
-class LinkedListDemo{
-	public static void main(String[] args) {
-		Link obj = new Link();
-		obj.insert(5);
-		obj.insert(6);
-		obj.insert(7);
-		obj.insert(8);
-		obj.insert(9);
-		obj.insert(10);
-		obj.display();
-		obj.insert(20);
-		obj.display();	
-	}
+    Node(int d) {
+      data = d;
+      next = null;
+    }
+  }
 
+  // Insert at the beginning
+  public void insertAtBeginning(int new_data) {
+    // insert the data
+    Node new_node = new Node(new_data);
+    new_node.next = head;
+    head = new_node;
+  }
+
+  // Insert after a node
+  public void insertAfter(Node prev_node, int new_data) {
+    if (prev_node == null) {
+      System.out.println("The given previous node cannot be null");
+      return;
+    }
+    Node new_node = new Node(new_data);
+    new_node.next = prev_node.next;
+    prev_node.next = new_node;
+  }
+
+  // Insert at the end
+  public void insertAtEnd(int new_data) {
+    Node new_node = new Node(new_data);
+
+    if (head == null) {
+      head = new Node(new_data);
+      return;
+    }
+
+    new_node.next = null;
+
+    Node last = head;
+    while (last.next != null)
+      last = last.next;
+
+    last.next = new_node;
+    return;
+  }
+
+  // Delete a node
+  void deleteNode(int position) {
+    if (head == null)
+      return;
+
+    Node temp = head;
+
+    if (position == 0) {
+      head = temp.next;
+      return;
+    }
+    // Find the key to be deleted
+    for (int i = 0; temp != null && i < position - 1; i++)
+      temp = temp.next;
+
+    // If the key is not present
+    if (temp == null || temp.next == null)
+      return;
+
+    // Remove the node
+    Node next = temp.next.next;
+
+    temp.next = next;
+  }
+
+  // Search a node
+  boolean search(Node head, int key) {
+    Node current = head;
+    while (current != null) {
+      if (current.data == key)
+        return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  // Sort the linked list
+  void sortLinkedList(Node head) {
+    Node current = head;
+    Node index = null;
+    int temp;
+
+    if (head == null) {
+      return;
+    } else {
+      while (current != null) {
+        // index points to the node next to current
+        index = current.next;
+
+        while (index != null) {
+          if (current.data > index.data) {
+            temp = current.data;
+            current.data = index.data;
+            index.data = temp;
+          }
+          index = index.next;
+        }
+        current = current.next;
+      }
+    }
+  }
+
+  // Print the linked list
+  public void printList() {
+    Node tnode = head;
+    while (tnode != null) {
+      System.out.print(tnode.data + " ");
+      tnode = tnode.next;
+    }
+
+  }
+
+  public static void main(String[] args) {
+    LinkedList listObj = new LinkedList();
+
+    listObj.insertAtEnd(1);
+    listObj.insertAtBeginning(2);
+    listObj.insertAtBeginning(3);
+    listObj.insertAtEnd(4);
+    listObj.insertAfter(llist.head.next, 5);
+
+    System.out.println("Linked list: ");
+    listObj.printList();
+
+    System.out.println("\nAfter deleting an element: ");
+    listObj.deleteNode(3);
+    listObj.printList();
+
+    System.out.println();
+    int item_to_find = 3;
+    if (listObj.search(listObj.head, item_to_find))
+      System.out.println(item_to_find + " is found");
+    else
+      System.out.println(item_to_find + " is not found");
+
+    listObj.sortLinkedList(listObj.head);
+    System.out.println("\nSorted List: ");
+    listObj.printList();
+  }
 }
