d19f216b927760133c053caf63452bf00f07aa05
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-01-04 20:52:16 +0530

    InfixToPostFix program

--- Stack/InfixToPostFix.java
+++ Stack/InfixToPostFix.java
@@ -1,71 +1,60 @@
-
-//Write program to convert Infix Expression to Post-Fix Expression / Reverse Polish Expression
 import java.util.Stack;
+import java.util.Scanner;
+public class InfixToPostfix {
+    static int Precedence(char ch){
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
 
-class InfixToPostFixImplement {
-    public boolean checkIfOpearator(char ch) {
-        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-            return true;
-        else
-            return false;
-    }
+            case '*':
+            case '/':
+                return 2;
 
-    public int precedance(char ch) {
-        if (ch == '+' || ch == '-') 
-            return 1;
-        else if (ch == '*' || ch == '/') 
-            return 2;
-        else if (ch == '^')
-            return 3;
-        else
+            case '^':
+                return 3;
+            }
             return -1;
     }
 
-    public void convertInfixToPostFix(String infix) {
+    static String InfixToPostFix(String infix) {
+        String postfix = new String("");
         Stack<Character> stack = new Stack<Character>();
-        StringBuilder revPolishExp = new StringBuilder(new String(""));
+        for (int i = 0; i < infix.length(); ++i) {
+            char ch = infix.charAt(i);
 
-        for (int i = 0; i < infix.length(); i++) {
-            char temp = infix.charAt(i);
-            if (checkIfOpearator(temp)) {
-                revPolishExp.append(temp);
-            } 
-            else if (temp == '(') {
-                stack.push(temp);
-            } 
-            else if (temp == '+' || temp == '-' || temp == '*' || temp == '/' || temp == '^') {
-                while (!stack.empty() && precedance(temp) <= precedance((char) stack.peek())) {
-                    revPolishExp.append(stack.peek());
+            if (Character.isLetterOrDigit(ch)) {
+                postfix += ch;
+            } else if (ch == '(') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix += stack.peek();
                     stack.pop();
                 }
-                stack.push(temp);
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && Precedence(ch) <= Precedence(stack.peek())) {
+                    postfix += stack.peek();
+                    stack.pop();
+                }
+                stack.push(ch);
             }
-            else if(temp == ')'){
-                while( !stack.empty() && stack.peek()!= '('){
-                    revPolishExp.append(stack.peek());
-                    stack.pop();
-                }
-                if(!stack.empty() && stack.peek()!= '('){
-                    return;
-                }
-                else{
-                    stack.pop();
-                }
-            } 
         }
-        while (!stack.empty()){
-            revPolishExp.append(stack.peek());
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(') {
+                return "Invalid Infix Expression";
+            }
+            postfix += stack.peek();
             stack.pop();
         }
-        System.out.println(revPolishExp);
+        return postfix;
     }
-}
 
-
-public class InfixToPostFix{
     public static void main(String[] args) {
-        String Infix = "(a+b-c)*(d/e+f*g)";
-        InfixToPostFixImplement obj = new InfixToPostFixImplement();
-        obj.convertInfixToPostFix(Infix);
+        System.out.println("Enter Infix string : ");
+        Scanner sc = new Scanner(System.in);
+        String infix_string = sc.nextLine();
+        System.out.println("Postfix : " + InfixToPostFix(infix_string));
     }
 }
