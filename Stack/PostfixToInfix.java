da27ec8727f2fb2f4eba6b2c714e55a7bd14814d
dhruvkaravadiya <karavadiadhruv22@gmail.com>
2023-06-26 10:45:25 +0530

    Added postfix to infix Algorithm comment

--- Stack/PostfixToInfix.java
+++ Stack/PostfixToInfix.java
@@ -1,3 +1,10 @@
+//ab+ => a+b
+//1. Read characters one by one
+//2. If character is opearand (alphabet) the push to the stack
+//3. Elsse pop to elements and then do 
+// op2 + operator + op1 and then push the result
+//4. keep doing this until the last element of the input string and 
+//5 At last pop the element which will be our answer 
 import java.util.Scanner;
 import java.util.Stack;
 
