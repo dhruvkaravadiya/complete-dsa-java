346df03c1e2e79a4710595d0e7be957a86614768
dhruvkaravadiya <karavadiadhruv22@gmail.com>
2023-06-28 19:57:53 +0530

    tempelate Node

---
+++ LinkedList/Implementations/Node.java
@@ -0,0 +1,13 @@
+package LinkedList.Implementations;
+
+public class Node {
+    // 'next' contains address/link to the next node
+        Node next;
+        // 'data' is the data storage part
+        int data;
+
+        Node(int data) {
+            this.next = null;
+            this.data = data;
+        }
+}
