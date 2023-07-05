35bc1a2684dc0ec280220eae6eef5d9bd7ef2633
dhruvkaravadiya <karavadiadhruv22@gmail.com>
2023-07-05 21:08:06 +0530

    initialize queue using linked list

---
+++ LinkedList/Implementations/queueUsingLinkedList.java
@@ -0,0 +1,26 @@
+package LinkedList.Implementations;
+class QueueUsingLinkedListImplementation{
+    Node rear;
+    Node front;
+    class Node {
+    // 'next' contains address/link to the next node
+        Node next;
+        // 'data' is the data storage part
+        int data;
+        Node(int data) {
+            this.next = null;
+            this.data = data;
+        }
+    }
+    void enqueue(){
+        //check if the queue is empty
+        if(rear == front){
+
+        }
+    }
+}
+class queueUsingLinkedList{
+    public static void main(String[] args) {
+        
+    }
+}
