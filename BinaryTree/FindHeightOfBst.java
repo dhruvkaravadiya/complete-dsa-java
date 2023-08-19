0dc59aed915f1cf1d563e586c5bd7b06fb43364b
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-08-19 21:35:21 +0530

    create Find Height of BST

---
+++ BinaryTree/FindHeightOfBst.java
@@ -0,0 +1,32 @@
+package BinaryTree;
+
+import utils.TreeNode;
+
+public class FindHeightOfBst {
+    public static int BSTHeight(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        else{
+            int leftTreeHeight = BSTHeight(node.left);
+            int rightTreeHeight =  BSTHeight(node.right);
+
+            if(leftTreeHeight < rightTreeHeight){
+                return leftTreeHeight + 1;
+            }
+            else{
+                return rightTreeHeight + 1;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        TreeNode btree = new TreeNode(1);
+        btree.left = new TreeNode(2);
+        btree.right = new TreeNode(3);
+        btree.left.left = new TreeNode(4);
+        btree.left.right = new TreeNode(5);
+        btree.right.left = new TreeNode(6);
+        btree.right.right = new TreeNode(7);
+        System.out.println(BSTHeight(btree));
+    }
+}
