35401680a33f5d065c9c27d8bbd2fab25e412c8b
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-27 17:47:03 +0530

    create groupAnagrams.java

---
+++ Array/GroupAnagrams.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class GroupAnagrams {
+      public static HashMap<String , ArrayList<String>> anagrams(String []input){
+            HashMap<String, ArrayList<String>> map = new HashMap<String , ArrayList<String>>();
+
+            for(int i = 0 ; i  < input.length ; i++){
+                  String str = input[i];
+                  char[] chrs = str.toCharArray();
+                  Arrays.sort(chrs);
+                  String mapKey = String.valueOf(chrs);
+                  ArrayList<String> list = map.getOrDefault(mapKey, new ArrayList<>());
+                  list.add(str);
+                  map.put(mapKey , list);
+            }
+            return map;
+      }
+      public static void main(String[] args) {
+            String[] strs = {"eat","tea","tan","ate","nat","bat"};
+            HashMap<String, ArrayList<String>> map = anagrams(strs);
+            for(String key: map.keySet()){
+                  ArrayList<String> list = map.get(key);
+                  System.out.println(Arrays.toString(list.toArray()));
+            }
+      }
+}
