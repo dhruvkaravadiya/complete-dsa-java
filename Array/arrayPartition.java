3c0fae0d9e5f5c8dde690e839bc565a39303f925
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-27 17:47:26 +0530

    create arrayPartition

--- Array/arrayPartition.java
+++ Array/arrayPartition.java
@@ -1,5 +1,32 @@
+//find the sum of all the min of sorted pairs of 2 numbers for each in array
+// example : 1,4,3,2 => min(1,2) + min(3,4) = 1 + 3 = 4
 public class arrayPartition {
-    public static void main(String[] args) {
-        
+    public static int[] bubbleSort(int arr[]){
+        int size = arr.length;
+        for(int i = 0 ; i < size ; i++){
+            boolean flag = false;
+            for(int j = 0 ; j < size - i - 1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    flag = true;
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                }
+            }
+            if(!flag){
+                break;
+            }
+        }
+        return arr;
     }
+    public static void main(String[] args) {
+        int arr[] = {1,4,3,2};
+        int arr2[] = bubbleSort(arr);
+        System.err.println(arr2[1]);
+        int sum = 0;
+        for(int i = 0 ; i < arr2.length-1 ; i+=2){
+            sum+=Math.min(arr2[i], arr2[i+1]);
+        }
+        System.out.println("Sum : "+sum);
+    }  
 }
