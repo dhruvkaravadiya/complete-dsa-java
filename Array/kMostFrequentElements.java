d2b9f7fb408d07aacb14ca94fd427e590d5ba840
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-27 17:47:46 +0530

    create kMostFrequentElements.java

---
+++ Array/kMostFrequentElements.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class kMostFrequentElements {
+      public static int[] getMostFrequentElements(int[] arr, int k) {
+            int[] result = new int[k];
+            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+            for (int i = 0; i < arr.length; i++) {
+                  map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            }
+            List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+            list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+
+            for (int i = 0; i < k && i < list.size(); i++) {
+                  result[i] = list.get(i).getKey();
+            }
+            return result;
+      }
+
+      public static void main(String[] args) {
+
+            int result[] = getMostFrequentElements(new int[]{1,1,1,2,2,3,3}, 2);
+            for (int i = 0; i < result.length; i++) {
+                  System.out.println(result[i]);
+            }
+      }
+}
