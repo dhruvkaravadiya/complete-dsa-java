a77b54c64969a40df39eaf2be619bb56d64c18f8
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2023-12-31 19:31:28 +0530

    valid palindrome string implemented

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class ValidPalindrome {
+      public static boolean isPalindrome(String s) {
+            int n = s.length();
+            s = s.replaceAll("\\s", "");
+            s = s.toLowerCase();
+            s = s.replaceAll("[^a-zA-Z0-9]", "");
+            int start = 0;
+            int end = s.length() - 1;
+            while (start < end) {
+                  if (s.charAt(start) != s.charAt(end)) {
+                        return false;
+                  }
+                  start++;
+                  end--;
+            }
+            return true;
+      }
+
+      public static void main(String[] args) {
+            System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+            System.out.println(isPalindrome("race a car"));
+      }
+}
