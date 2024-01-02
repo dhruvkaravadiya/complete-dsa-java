fae6a20120e0b56f391c0ac9ee88d07db916a909
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-01-02 15:29:36 +0530

    create validParenthesis.java

---
+++ Two_Pointers/ValidParenthesis.java
@@ -0,0 +1,55 @@
+package Two_Pointers;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+      public static boolean isValid(String s) {
+            if (s.length() == 1) {
+                  return false;
+            }
+            Stack<Character> stk = new Stack<>();
+            boolean flag = true;
+            for (int i = 0; i < s.length(); i++) {
+                  char ch = s.charAt(i);
+                  if (ch == '(' || ch == '[' || ch == '{') {
+                        stk.push(ch);
+                  } else if (ch == ']') {
+                        if (!stk.isEmpty() && stk.peek() == '[') {
+                              stk.pop();
+
+                        } else {
+                              flag = false;
+                              break;
+                        }
+                  } else if (ch == '}') {
+                        if (!stk.isEmpty() && stk.peek() == '{') {
+                              stk.pop();
+                        } else {
+                              flag = false;
+                              break;
+                        }
+                  } else if (ch == ')') {
+                        if (!stk.isEmpty() && stk.peek() == '(') {
+                              stk.pop();
+                        } else {
+                              flag = false;
+                              break;
+                        }
+                  }
+            }
+            return flag && stk.isEmpty();
+      }
+
+      public static void main(String[] args) {
+            System.out.println("1. (()) - " + isValid("(())")); // true
+            System.out.println("2. ({[()]}) - " + isValid("({[()]})")); // true
+            System.out.println("3. {[()]}) - " + isValid("{[()]})")); // false
+            System.out.println("4. ((){}) - " + isValid("((){})")); // false
+            System.out.println("5. [(]) - " + isValid("[(])")); // false
+            System.out.println("6. (){}[] - " + isValid("(){}[]")); // true
+            System.out.println("7. ({[)]}) - " + isValid("({[)]})")); // false
+            System.out.println("8. (()[{}]) - " + isValid("(()[{}])")); // true
+            System.out.println("9. {[()]}( - " + isValid("{[()]}(")); // false
+            System.out.println("10. (()[{}])() - " + isValid("(()[{}])()")); // true
+      }
+}
