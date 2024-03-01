b79433ea486d47f9ef73fc6a136fc94de98f6101
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-03-01 15:42:16 +0530

    Add FirstMissingPositive class

---
+++ Binary_Search/FirstMissingPositive.java
@@ -0,0 +1,12 @@
+package Binary_Search;
+
+// public class FirstMissingPositive {
+// public static void main(String[] args) {
+// int[] arr = { 3, 4, -1, 1 };
+// System.out.println(firstMissingPositive(arr));
+// }
+
+// public static int FirstMissingPositive(int[] nums) {
+
+// }
+// }
