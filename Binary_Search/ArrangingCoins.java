06f369ea7ec0cc3078fe4f2c66ea08b1952ce9ee
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-03-01 15:41:51 +0530

    Add ArrangingCoins class to Binary_Search package

---
+++ Binary_Search/ArrangingCoins.java
@@ -0,0 +1,15 @@
+package Binary_Search;
+
+class Solution {
+      public int arrangeCoins(int n) {
+            int i = 1;
+            int count = 0;
+            while (i <= n) {
+                  n -= i;
+                  i++;
+                  count++;
+            }
+            return count;
+      }
+
+}
