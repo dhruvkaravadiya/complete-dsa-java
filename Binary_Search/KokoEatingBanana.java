8b58285da1b99f5cd511920ee09c1a4d1c563a44
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-03-01 15:42:32 +0530

    Add KokoEatingBanana class for binary search algorithm

---
+++ Binary_Search/KokoEatingBanana.java
@@ -0,0 +1,28 @@
+package Binary_Search;
+
+import java.util.Arrays;
+
+class Solution {
+      public int minEatingSpeed(int[] piles, int h) {
+            int left = 1;
+            int right = Arrays.stream(piles).max().getAsInt();
+
+            while (left <= right) {
+                  int mid = left + (right - left) / 2;
+                  if (canEatBananas(mid, piles, h)) {
+                        right = mid;
+                  } else {
+                        left = mid + 1;
+                  }
+            }
+            return left;
+      }
+
+      public boolean canEatBananas(int mid, int[] piles, int h) {
+            int hours = 0;
+            for (int pile : piles) {
+                  hours += (pile + mid - 1) / mid;
+            }
+            return hours <= h;
+      }
+}
