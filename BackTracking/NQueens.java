365f0c06383153e208f86a4a3262fbef7f392641
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-03-17 19:47:28 +0530

    Rename NQueens class to NQueens1

--- BackTracking/NQueens.java
+++ BackTracking/NQueens.java
@@ -6,7 +6,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class NQueens {
+public class NQueens1 {
       public static void main(String[] args) {
             int n = 4;
             List<List<String>> result = new ArrayList<>();
