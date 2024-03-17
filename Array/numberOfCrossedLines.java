9c259b249842e0658fe51d0a7bb162381bb90f4a
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-03-17 19:47:18 +0530

    Refactor maxUncrossedLines method and fix formatting issues

--- Array/numberOfCrossedLines.java
+++ Array/numberOfCrossedLines.java
@@ -1,39 +1,36 @@
-class Solution {
-    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return lcs(nums1 , nums2);
+public class numberOfCrossedLines {
+    public static int maxUncrossedLines(int[] nums1, int[] nums2) {
+        return lcs(nums1, nums2);
     }
-    public static int lcs(int[] s1 ,int[] s2){
+
+    public static int lcs(int[] s1, int[] s2) {
         int m = s1.length;
         int n = s2.length;
-        int [][]arr = new int[m+1][n+1];
+        int[][] arr = new int[m + 1][n + 1];
         int maxLen = 0;
-        for(int i = 0 ; i <= m ; i++){
-            for(int j = 0 ; j <= n ;j++){
-                if(i==0 || j==0){
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
                     arr[i][j] = 0;
-                }
-                else if(s1[i-1] == s2[j-1]){
-                    
-                    arr[i][j] = arr[i-1][j-1] + 1;
-                    
-                    if(arr[i][j] > maxLen) {
+                } else if (s1[i - 1] == s2[j - 1]) {
+
+                    arr[i][j] = arr[i - 1][j - 1] + 1;
+
+                    if (arr[i][j] > maxLen) {
                         maxLen = arr[i][j];
-                        //ans = s1.subint(i-maxLen, i);
+                        // ans = s1.subint(i-maxLen, i);
                     }
-                }
-                else{
-                    arr[i][j] = ((arr[i-1][j] > arr[i][j-1]) ? arr[i-1][j]:arr[i][j-1]) ;
+                } else {
+                    arr[i][j] = ((arr[i - 1][j] > arr[i][j - 1]) ? arr[i - 1][j] : arr[i][j - 1]);
                 }
             }
         }
-        return maxLen; 
+        return maxLen;
     }
-}
-public class numberOfCrossedLines{
+
     public static void main(String[] args) {
-        Solution s = new Solution();
-        int a1[]={1,3,7,1,7,5};
-        int a2[] = {1,9,2,5,1};
-        System.out.println(s.maxUncrossedLines(a1, a2));
+        int a1[] = { 1, 3, 7, 1, 7, 5 };
+        int a2[] = { 1, 9, 2, 5, 1 };
+        System.out.println(maxUncrossedLines(a1, a2));
     }
 }
