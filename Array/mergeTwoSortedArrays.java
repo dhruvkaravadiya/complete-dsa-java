a1893a2f3ca1f158b09075e3afd0ff6b768b88f5
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-04-23 10:40:08 +0530

    Refactor mergeTwoSortedArrays.java for improved readability and maintainability

--- Array/mergeTwoSortedArrays.java
+++ Array/mergeTwoSortedArrays.java
@@ -1,34 +1,40 @@
 public class mergeTwoSortedArrays {
     public static void main(String[] args) {
-        int a1[] = {1,2,3,4,5,6};
-        int a2[] = {3,4,5,6,7,8};
+        int a1[] = { 1, 2, 3, 4, 5, 6 };
+        int a2[] = { 3, 4, 5, 6, 7, 8 };
         int x = a1.length, y = a2.length;
         int result[] = new int[x + y];
-        int i = 0 , j = 0 , k = 0;
-        while(i < x && j < y ){
-            if(a1[i] < a2[j]){
+        int i = 0, j = 0, k = 0;
+        while (i < x && j < y) {
+            if (a1[i] < a2[j]) {
                 result[k] = a1[i];
                 i++;
                 k++;
-            }
-            else{
+            } else if (a1[i] > a2[j]) {
                 result[k] = a2[j];
                 k++;
                 j++;
+            } else {
+                result[k] = a1[i];
+                i++;
+                j++;
+                k++;
             }
         }
-        while(i < x){
+        while (i < x)
+
+        {
             result[k] = a1[i];
             k++;
             i++;
         }
-        while(j < y){
+        while (j < y) {
             result[k] = a2[j];
             k++;
             j++;
         }
-        for(int z = 0 ; z < x+y ; z++){
-            System.out.print(result[z]+" ");
+        for (int z = 0; z < x + y; z++) {
+            System.out.print(result[z] + " ");
         }
     }
 }
