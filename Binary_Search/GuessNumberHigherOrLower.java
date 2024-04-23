b6c6665338263838acb2a4678eb5e078e3ac0d31
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-04-23 10:40:47 +0530

    Refactor GuessNumberHigherOrLower.java for improved readability and maintainability

--- Binary_Search/GuessNumberHigherOrLower.java
+++ Binary_Search/GuessNumberHigherOrLower.java
@@ -11,23 +11,21 @@ public class GuessNumberHigherOrLower {
 // * int guess(int num);
 // */
 
-// public class Solution extends GuessGame {
-// public int guessNumber(int n) {
-// int left = 1;
-// int right = n;
-// while(left<=right){
-// int mid = left + (right - left)/2;
-// int res = guess(mid);
-// if(res == -1){
-// right = mid-1;
-// }
-// else if(res == 1){
-// left = mid+1;
-// }
-// else{
-// return mid;
-// }
-// }
-// return -1;
-// }
-// }
+public class Solution extends GuessGame {
+      public int guessNumber(int n) {
+            int left = 1;
+            int right = n;
+            while (left <= right) {
+                  int mid = left + (right - left) / 2;
+                  int res = guess(mid);
+                  if (res == -1) {
+                        right = mid - 1;
+                  } else if (res == 1) {
+                        left = mid + 1;
+                  } else {
+                        return mid;
+                  }
+            }
+            return -1;
+      }
+}
