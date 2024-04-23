8035496a4ffc705f1a8589eb99d3fa4aaaf8b21d
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-04-23 10:41:26 +0530

    Refactor kMostFrequentElements.java

--- Hashing/kMostFrequentElements.java
+++ Hashing/kMostFrequentElements.java
@@ -1,4 +1,5 @@
 package Hashing;
+
 import java.util.ArrayList;
 // import java.util.Arrays;
 import java.util.HashMap;
@@ -10,7 +11,7 @@ public class kMostFrequentElements {
             int[] result = new int[k];
             HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
             for (int i = 0; i < arr.length; i++) {
-                  map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+                  map.put(arr[i], map.getOrDefault(arr[i], 1) + 1);
             }
             List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
             list.sort((a, b) -> b.getValue().compareTo(a.getValue()));
@@ -23,9 +24,9 @@ public class kMostFrequentElements {
 
       public static void main(String[] args) {
 
-            int result[] = getMostFrequentElements(new int[]{1,1,1,2,2,3,3}, 2);
+            int result[] = getMostFrequentElements(new int[] { 1, 1, 1, 2, 2, 3, 3 }, 2);
             for (int i = 0; i < result.length; i++) {
-                  System.out.println(result[i]);
+                  System.out.print(result[i] + " ");
             }
       }
 }
