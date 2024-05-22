c6ecf3215b920d6a11c63a76ac625f06c996aa10
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-05-22 09:45:32 +0530

    update bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,34 +2,35 @@
 //Algo : Compare two elements , if the first is greater,then
 // swap it  with second , thus at the end of one for loop iteration 
 //greatest will be at the end  , and the remaining array remains intaact
-package Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] = {10,9,8,7,6,5,4,3,2,1};
-        int size = arr.length;
+        int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        int n = arr.length;
         System.out.print("Before : ");
-        for(int i = 0 ; i < size ; i++ ){
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-        for(int i = 0 ; i < size ; i++){
+
+        for (int i = 0; i < n; i++) {
             boolean flag = false;
-            for(int j = 0 ; j < size - i - 1 ; j++){
-                if(arr[j] > arr[j+1]){
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     flag = true;
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
-                arr[j] = temp;
                 }
+
             }
-            //for sorted  , it will break , 
-            //thus no more unnecessary iterations
-            if(!flag){
+            if (!flag) {
                 break;
             }
+
         }
+
         System.out.print("\nAfter Bubble Sort : ");
-        for(int i = 0 ; i < size ; i++ ){
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
