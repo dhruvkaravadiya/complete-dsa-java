df3bbcf346e52a891a02ff1cd9eaeb52e1b8ad0e
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-05-22 09:45:59 +0530

    update insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -3,11 +3,11 @@
 // it is found.
 // For descending order, change key<array[j] to key>array[j].
 // Place key at after the element just smaller than it
-package Sorting;
 
 public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
         int size = arr.length;
         System.out.print("Before : ");
         for (int i = 0; i < size; i++) {
@@ -17,12 +17,10 @@ public class InsertionSort {
         for (int i = 1; i < size; i++) {
             int key = arr[i];
             int j = i - 1;
-
             while (j >= 0 && key < arr[j]) {
                 arr[j + 1] = arr[j];
-                --j;
+                j--;
             }
-
             arr[j + 1] = key;
         }
 
