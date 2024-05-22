6f9b2ef52f6f3f15a85bed699d71a6d61b7f1046
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-05-22 09:46:05 +0530

    update selection sort

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,34 +1,35 @@
-//implement selection sort
-//Algo : With each iteration the smallest element is 
-//       placed at the first index of the unsorted array
-package Sorting;
+/**
+ * The selection sort algorithm sorts an array by repeatedly finding the minimum
+ * element from the unsorted part of the array and placing it at the beginning.
+ * It divides the array into two parts: the sorted part and the unsorted part.
+ * Initially, the sorted part is empty and the unsorted part is the entire
+ * array.
+ * The algorithm then selects the smallest element from the unsorted part and
+ * swaps it with the leftmost element of the unsorted part.
+ * This process continues until the entire array is sorted.
+ */
+
 public class SelectionSort {
     public static void main(String[] args) {
-        int arr[] = {10,9,8,7,6,5,4,3,2,1};
-        int size = arr.length;
-        System.out.print("Before : ");
-        for(int i = 0 ; i < size ; i++ ){
-            System.out.print(arr[i] + " ");
-        }
-        
-        for(int i = 0 ; i < size ; i++){
+        int arr[] = { 64, 25, 12, 22, 11 };
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
             int min = arr[i];
             int minIndex = i;
-
-            for(int j = i; j < size ; j++){
-                if(min > arr[j]){
+            for (int j = i + 1; j < n; j++) {
+                if (min > arr[j]) {
                     min = arr[j];
                     minIndex = j;
                 }
-            }   
-           int temp =  arr[minIndex];
-           arr[minIndex] = arr[i];
-           arr[i] = temp;
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
 
-        System.out.print("\nAfter Selection Sort: ");
-        for(int i = 0 ; i < size ; i++ ){
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
+
     }
 }
