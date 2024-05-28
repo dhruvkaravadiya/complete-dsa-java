b995ae265bfbd93b86bbb6d5597a9811aa543a43
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-05-28 08:28:20 +0530

    update leadersInAnArray.java

--- Array/LeadersInAnArray.java
+++ Array/LeadersInAnArray.java
@@ -1,16 +1,15 @@
 public class LeadersInAnArray {
     public static void main(String[] args) {
-        int nums[] = {17,18,5,2,6,1};
+        int nums[] = { 17, 18, 5, 2, 6, 1 };
         int n = nums.length;
-        if(nums.length == 1){
-            System.out.print(nums[0]);
-        }
-        else{
-            int max = nums[n-1];
-            for(int i = n-2 ; i>=0 ; i--){
-                if(nums[i] > max){   
+        if (nums.length == 1) {
+            System.out.println(nums[0]);
+        } else {
+            int max = nums[n - 1];
+            for (int i = n - 1; i >= 0; i--) {
+                if (max <= nums[i]) {
                     max = nums[i];
-                    System.out.print(max+" ");
+                    System.out.print(max + " ");
                 }
             }
         }
