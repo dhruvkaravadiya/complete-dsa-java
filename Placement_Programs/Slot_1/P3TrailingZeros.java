9a3632767c69d3b991cb03a4a2620f69fda31e59
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-06-28 18:54:26 +0530

    add practise programs

--- Placement_Programs/Slot_1/P3TrailingZeros.java
+++ Placement_Programs/Slot_1/P3TrailingZeros.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 import java.math.BigInteger;
+
 public class P3TrailingZeros {
     public static void main(String[] args) {
         System.out.println("Enter Number : ");
         Scanner sc = new Scanner(System.in);
-        //int n = sc.nextInt();
+
+        // int n = sc.nextInt();
         int n = 12;
-        //int n2 = 30;
+        // int n2 = 30;
         String res = factorial(BigInteger.valueOf(n)).toString();
         System.out.println(res);
         int count = 0;
-        for(int i = res.length()-1 ; i>=0 ; i--){
-            if(res.charAt(i) != '0'){
-               
+        for (int i = res.length() - 1; i >= 0; i--) {
+            if (res.charAt(i) != '0') {
+
                 break;
-            }
-            else if(res.charAt(i) == '0'){
+            } else if (res.charAt(i) == '0') {
                 count++;
             }
         }
         System.out.println(count);
     }
 
-    public static BigInteger factorial(BigInteger n){
+    public static BigInteger factorial(BigInteger n) {
         if (n.compareTo(BigInteger.ZERO) <= 0) {
             return BigInteger.ONE;
         } else {
--- Placement_Programs/Slot_2/P1_BinaryToDecimal.java
+++ Placement_Programs/Slot_2/P1_BinaryToDecimal.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
-public class P1_BinaryToDecimal{
+public class P1_BinaryToDecimal {
     public static void main(String[] args) {
         double num = 101.110;
-        String[] strs =  Double.toString(num).split("\\."); 
+        String[] strs = Double.toString(num).split("\\.");
         System.out.println(strs[0]);
-        System.out.println(strs[1]);  
-        double leftPart = productSum(strs[0], strs[0].length()-1, 0);
+        System.out.println(strs[1]);
+        double leftPart = productSum(strs[0], strs[0].length() - 1, 0);
         double rightPart = productSum(strs[1], (strs[1].length() - 1) * (-1), strs[1].length() * (-1));
-        //System.out.println(leftPart);
-        //System.out.println(rightPart);
+        // System.out.println(leftPart);
+        // System.out.println(rightPart);
         double decimalNum = leftPart + rightPart;
         System.out.println(decimalNum);
     }
-    public static int productSum(String str , int l , int r){
+
+    public static int productSum(String str, int l, int r) {
         int res = 0;
         int x = 0;
-        for(int i = l ; i >= r ; i--){
+        for (int i = l; i >= r; i--) {
             System.out.println(str.charAt(x));
-            if(str.charAt(x) == '1'){
-                res +=  Math.pow(2, l ); 
-                x++; 
-            }    
+            if (str.charAt(x) == '1') {
+                res += Math.pow(2, l);
+                x++;
+            }
         }
         return res;
     }
